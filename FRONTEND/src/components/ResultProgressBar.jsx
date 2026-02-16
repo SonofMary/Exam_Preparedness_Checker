af2924cc29fc9decf8381a632349a83de39d7b7d
SonofMary <izuchukwumarius@gmail.com>
2026-02-16 12:52:44 +0100

    Update ResultProgressBar

--- FRONTEND/src/components/ResultProgressBar.jsx
+++ FRONTEND/src/components/ResultProgressBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ResultProgressBar = ({percentage, difficulty, }) => {
   return (
     <div className="w-full">
-        <progress className={`progress w-full sm:w-96 h-4 sm:h-6 ${difficulty === `easy` && `progress-success`} ${difficulty === `medium` && `progress-warning`} ${difficulty === `hard` && `progress-error`} ${difficulty === `advanced` && `progress-error`}`}  max="100" min="0" value={percentage}></progress>
+        <progress className={`progress w-36 sm:w-96 h-4 sm:h-6 ${difficulty === `easy` && `progress-success`} ${difficulty === `medium` && `progress-warning`} ${difficulty === `hard` && `progress-accent`} ${difficulty === `advanced` && `progress-error`}`}  max="100" min="0" value={percentage}></progress>
     </div>
   )
 }
